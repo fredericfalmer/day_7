deb524183a72df489b43cabac8b78259b8c87367
Frederic Falmer <frederic.falmer@gmail.com>
2020-04-07 12:48:23 +0200

    exo_13 avec une seule variable

--- exo_13.rb
+++ exo_13.rb
@@ -1,8 +1,6 @@
 puts "Bonjour, quelle est ta date de naissance ? "
 var_birth = gets.chomp.to_i
 
-var_iterations = 2020 - var_birth
-var_iterations.times do |i|
-		puts "#{var_birth+i+1}"
-	end
-
+(2020 - var_birth).times do |i|
+	puts "#{var_birth + i + 1}"
+end
